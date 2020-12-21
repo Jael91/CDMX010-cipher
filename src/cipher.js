d9854de8bdc0d06caf22ff01f79ab19f8ea23e1b
Jael Orea <jaelorea@MacBook-Pro-de-Jael.local>
2020-12-21 09:48:03 -0600

    add all changes

--- src/cipher.js
+++ src/cipher.js
@@ -1,32 +1,25 @@
 const cipher = {
-        encode (displacementInNumber,secretMessage){   
+    encode (offset, secretMessage){   
             let result;
             let array=[]; 
-            for(let i = 0;i<secretMessage.length;i++){ 
+            for(let i = 0;i<secretMessage.length;i++){ // jqnc, 24
                 if(secretMessage.charCodeAt(i)>=65 && secretMessage.charCodeAt(i)<=90){
-                    result = String.fromCharCode((secretMessage.charCodeAt(i)-65 + displacementInNumber)%26+65);
+                    result = String.fromCharCode((secretMessage.charCodeAt(i)-65 + offset)%26+65);
                     array.push(result)
             }
                         else if(secretMessage.charCodeAt(i) >= 97 && secretMessage.charCodeAt(i)<=122){
-                        result=String.fromCharCode((secretMessage.charCodeAt(i)-97+displacementInNumber)%26+97);
-                        array.push(result)
-                        console.log(array)
+                        result=String.fromCharCode((secretMessage.charCodeAt(i)-97+offset)%26+97);
+                        array.push(result);
                 }
         
         }
         return array.join(""); 
     },
-        decode (displacementInNumber2,secretMessage2){
-            let result2;
-            let array2=[];
-            for(let i = 0;i<secretMessage2.length;i++){
-                if(secretMessage2.charCodeAt(i)>= 65 && secretMessage2.charCodeAt(i)<=90){
-                    console.log(secretMessage2.charCodeAt(i))
-                    result2 = String.fromCharCode((secretMessage2.charCodeAt(i)-65 - displacementInNumber2)%26+65);
-                    array2.push(result2)
-                }
-            }
-        return array2.join(""); 
+    decode (offset, secretMessage) {
+        const newOffset = 26 - (offset % 26);
+        const result = cipher.encode(newOffset, secretMessage);
+
+        return result;
     },
 };
 
--- src/index.js
+++ src/index.js
@@ -11,13 +11,17 @@ button.addEventListener("click", function(){
         let result = cipher.encode(parseInt(displacementInNumber.value),secretMessage.value);
         console.log(result);
         document.getElementById("final-image").innerHTML=result;
+        document.getElementById("resultadofinal").value = result;
 });
 
 let button2 = document.getElementById("button-02");
-let secretMessage2 = document.getElementById("newMessage");
-let displacementInNumber2 = document.getElementById("displacementUser");
+//let secretMessage2 = document.getElementById("final-image");
 
-button2.addEventListener("click", function(){
-        let result2 = cipher.decode(parseInt(displacementInNumber2.value),secretMessage2.value);
-        document.getElementById("final-image2").innerHTML=result2;
+button2.addEventListener("click", function() {
+        const offsetFromInput = document.getElementById("displacementUser").value;
+        const encodedMessage = document.getElementById("resultadofinal").value;
+        const offset = parseInt(offsetFromInput);
+        const result = cipher.decode(offset, encodedMessage);
+        
+        document.getElementById("final-image2").innerHTML=result;
 });
