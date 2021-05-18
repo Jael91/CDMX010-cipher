a3462a8188fea5b7b090376f3d0f740fc809ae4d
Jael91 <jaelorea@gmail.com>
2021-05-18 11:59:41 -0500

    adding css final changes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import cipher from './cipher.js';
 
 let button = document.getElementById("button-01");
 let secretMessage = document.getElementById("newMessage");
-console.log(secretMessage);
 let displacementInNumber = document.getElementById("displacementUser");
-console.log(displacementInNumber);
-console.log(button);
 
 button.addEventListener("click", function(){
         let result = cipher.encode(parseInt(displacementInNumber.value),secretMessage.value);
-        console.log(result);
-        document.getElementById("final-image").innerHTML=result;
-        document.getElementById("resultadofinal").value = result;
+        document.getElementById("code-result").innerHTML= result;
+        document.getElementById("finalresult").value = result;
 });
 
 let button2 = document.getElementById("button-02");
@@ -19,9 +15,9 @@ let button2 = document.getElementById("button-02");
 
 button2.addEventListener("click", function() {
         const offsetFromInput = document.getElementById("displacementUser").value;
-        const encodedMessage = document.getElementById("resultadofinal").value;
+        const encodedMessage = document.getElementById("finalresult").value;
         const offset = parseInt(offsetFromInput);
         const result = cipher.decode(offset, encodedMessage);
         
-        document.getElementById("final-image2").innerHTML=result;
+        document.getElementById("decode-result").innerHTML=result;
 });
